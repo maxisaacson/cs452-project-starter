37935f2c76c0346e16f036b6cb41ced510ec56ea
Max I <100453982+maxisaacson@users.noreply.github.com>
2024-09-12 18:47:57 +0000

    Added starter code

--- app/main.c
+++ app/main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
   printf("hello world\n");
+  int c;
+  while ((c = getopt(argc, argv, "abc:")) != -1)
+    switch(c)
+    {
+      case 'a':
+        printf("got A");
+        break;
+      case 'b':
+        printf("got B");
+        break;
+      case 'c':
+        printf("got C");
+        break;
+      case '?':
+        if (isprint(optopt))
+          fprintf(stderr, "Unknown option", optopt);
+        else
+          fprintf(stderr, "Unknown option", optopt);
+      return 1;
+      default:
+        abort();
+        break;
+    }
   return 0;
 }
